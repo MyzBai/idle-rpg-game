bf2f11249bd9ad67eaa53ef97f546e0a66acc553
Myzzie <Myzzie@protonmail.com>
2022-04-02 13:35:36 +0200

    return string id for removal

--- eventListener.js
+++ eventListener.js
@@ -1,28 +1,33 @@
+import { uuidv4 } from "./helperFunctions.js";
 
 var instances = [];
 export const EventType = {
-    LEVEL_UP: 1,
-    ENEMY_KILLED: 2,
-    ESSENCE_CHANGED: 3,
-    ITEM_CHANGED: 4
-}
-Object.freeze(EventType)
+	LEVEL_UP: 1,
+	ENEMY_KILLED: 2,
+	ESSENCE_CHANGED: 3,
+	ITEM_CHANGED: 4,
+};
+Object.freeze(EventType);
 
-/**@returns {function} remove function */
-export function add(type, callback){
-    const instance = {
-        type,
-        callback
-    };
-    instances.push(instance);
-    return () => {
-        instances = instances.filter(x => x !== instance);
-    }
+/**@returns {string} id */
+export function add(type, callback) {
+	const instance = {
+		type,
+		callback,
+	};
+    const id = uuidv4();
+    instance.id = id;
+	instances.push(instance);
+    return id;
 }
 
-export function invoke(type, ...params){
-    for (const instance of instances.filter(x => x.type === type)) {
-        instance.callback(...params);
-    }
- 
+/**@param {string} id */
+export function remove(id){
+    instances = instances.filter(x => x.id !== id);
+}
+
+export function invoke(type, ...params) {
+	for (const instance of instances.filter((x) => x.type === type)) {
+		instance.callback(...params);
+	}
 }
